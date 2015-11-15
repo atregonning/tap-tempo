ffa6e28736183f2e4905d5487127044ddb1290f5
Adrian Tregonning <atregonning@gmail.com>
2015-11-14 21:11:06 -0500

    Initial Commit

---
+++ TapTempo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TapTempo
+//
+//  Created by Adrian Tregonning on 14/11/2015.
+//  Copyright © 2015 Adrian Tregonning. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
